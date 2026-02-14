f632e38fe53144bb8a0a0df2b88f31ac07b816fd
JonathanSlacks <jobaraja@usc.edu>
2026-02-13 17:42:59 -0800

    Created scripts for movement, pacman, and pellet behaviors

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+    public float speed = 8f;
+    public float speedMultiplier = 1f;
+    public Vector2 initialDirection;
+    public LayerMask obstacleLayer;
+    public Vector2 direction { get; private set; }
+    public Vector2 nextDirection { get; private set; }
+    public Vector3 startingPosition { get; private set; }
+    public Rigidbody2D rb { get; private set; }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        startingPosition = transform.position;
+    }
+
+    private void Start()
+    {
+        ResetState();
+    }
+
+    public void ResetState()
+    {
+        speedMultiplier = 1f;
+        direction = initialDirection;
+        nextDirection = Vector2.zero;
+        transform.position = startingPosition;
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        enabled = true;
+    }
+
+    private void Update()
+    {
+        if (nextDirection != Vector2.zero)
+        {   
+            SetDirection(nextDirection);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rb.position;
+        Vector2 translation = direction * speed * speedMultiplier * Time.fixedDeltaTime;
+        
+        rb.MovePosition(position + translation);
+    }
+    public void SetDirection(Vector2 direction, bool forced = false)
+    {
+        if(forced || !Occupied(direction))
+        {
+            this.direction = direction;
+            nextDirection = Vector2.zero;
+        }
+        else
+        {
+            nextDirection = direction;
+        }
+    }
+
+    public bool Occupied(Vector2 direction)
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(this.transform.position, Vector2.one * 0.75f, 0f, direction, 1.5f, this.obstacleLayer);
+        return hit.collider != null;
+    }
+}
---
+++ Assets/Scripts/Pellet.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Pellet : MonoBehaviour
+{
+    public int points = 10;
+
+    protected virtual void Eat()
+    {
+        FindFirstObjectByType<GameManager>().PelletEaten(this);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Pacman")) ;
+        {
+            Eat();
+        }
+    }
+}
